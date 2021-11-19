9f45793dc52850270aef7a471a29905833e68779
Yusuf Vahit Kurtoğlu <93702740+Yusufx01@users.noreply.github.com>
2021-11-19 17:49:08 +0300

    Add files via upload

---
+++ 6_unite/Daire diliminin alanı.py
@@ -0,0 +1,5 @@
+a=float(input("dereceyi giriniz"));
+r=float(input("yarı çapı giriniz"));
+p=(3.14);
+alan=a*p*(r*r)/360
+print(alan)
---
+++ 6_unite/Dairenin alanı.py
@@ -0,0 +1,6 @@
+pi=3.14
+r=float(input("dairenin yarı çapını giriniz"));
+A=(4*pi*(r*r));
+print("A=%f"%A)
+
+
