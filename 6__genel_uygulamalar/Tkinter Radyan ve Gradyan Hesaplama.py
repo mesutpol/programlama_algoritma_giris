cb265f77b153a861c71e25097fad429b9a21de85
Orçun Buğra Özel <93700345+bugraozel@users.noreply.github.com>
2021-11-20 19:27:48 +0300

    Update Tkinter Radyan ve Gradyan Hesaplama.py

--- 6__genel_uygulamalar/Tkinter Radyan ve Gradyan Hesaplama.py
+++ 6__genel_uygulamalar/Tkinter Radyan ve Gradyan Hesaplama.py
@@ -4,21 +4,17 @@ def hesap():
     Radyan=a*(3/180)
     Gradyan=a*200/180
     
-    etiket2.config(text=str(Radyan))
-    etiket3.config(text=str(Gradyan))
+    from tkinter import messagebox
+    messagebox.showinfo("Radyan cinsinden",Radyan)
+    messagebox.showinfo("Gradyan cinsinden",Gradyan)
     
 root=tkinter.Tk()
 
-etiket1=tkinter.Label(root,text="Dereceyi Giriniz")
-etiket1.grid(row=0,column=1,padx=110,pady=10)
-
-etiket2=tkinter.Label(root,text="Radyan Cinsinden Sonucu")
-etiket2.grid(row=2,column=0,padx=110,pady=10)
-etiket3=tkinter.Label(root,text="Gradyan Cinsinden Sonucu")
-etiket3.grid(row=4,column=0,padx=110,pady=10)
+etiket1=tkinter.Label(root,text="Derece Giriniz")
+etiket1.grid(row=0,column=0,padx=110,pady=10)
 
 E1=tkinter.Entry(root,bd=2)
-E1.grid(row=3,column=1,padx=110,pady=3)
+E1.grid(row=3,column=0,padx=110,pady=3)
 
 button1=tkinter.Button(root,text="Hesapla",width=20,command=hesap)
 button1.grid(padx=110,pady=70)
