b1fa095b0a208fbc31bb425b88c1ab2485124173
mesut polatgil <93216327+mepol58@users.noreply.github.com>
2021-11-26 20:46:59 +0300

    Delete 6_74_xsayısınıe^x_olarakyazmak.py

--- 6_74_xsayısınıe^x_olarakyazmak.py
+++
@@ -1,7 +0,0 @@
-import math
-N=int(input("terim sayısını giriniz."))
-x=eval(input("hesaplanacak x değerini giriniz: ")) ; T=1; F=1
-for i in range (1,N):
-    F*=i; T+=x**i/F
-print("\nSeri açılımı ile e üzeri x değeri: %0.5f"%T)
-print("komumtla e üzeri x değeri: %0.5f"%math.exp(x))
