618d49d43bd82697dbfa344f639336c26c7e2eaa
ahmetcantas60 <93476286+ahmetcantas60@users.noreply.github.com>
2021-12-30 18:56:32 +0300

    Create 7_29_büyükse küçük küçükse büyük yaparak yeniden yazdıran program.py

---
+++ 7_bir_boyutlu_dizi/7_29_büyükse küçük küçükse büyük yaparak yeniden yazdıran program.py
@@ -0,0 +1,7 @@
+# Harfin tersi
+k=input("kelime: ")
+for i in range (len(k)):
+  if ((ord(k[i])>=65) & (ord(k[i])<=90)):
+    print("%c"%(ord(k[i])+32,end='')
+          else:
+          print("%c"(ord(k[i])-32),end='')
