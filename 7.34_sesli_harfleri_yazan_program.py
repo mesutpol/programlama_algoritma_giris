2ca1a8db73f397d45007dcbde41a446dcc532a56
mesut polatgil <93216327+mepol58@users.noreply.github.com>
2021-12-30 14:48:54 +0300

    Delete 7.34_sesli_harfleri_yazan_program.py

--- 7.34_sesli_harfleri_yazan_program.py
+++
@@ -1,5 +0,0 @@
-b="aeıioöuü" ; s=0 ; yer=[] ; c=input("cümle: ")
-for i in range(len(c)) :
-    if (c[i] in b) :
-        s+=1; yer.append(i+1)
-print("\nCümlede %d tane sesli harf vardır. \nYerleri:"%s) ; print(yer)
