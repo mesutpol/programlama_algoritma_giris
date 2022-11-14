cd49a8f573e3653d9e5dd495867168dbfc500c56
Emre Bolat <115585305+Emre556115@users.noreply.github.com>
2022-11-14 13:44:12 +0300

    Add files via upload

---
+++ 10_dosyalama_uyg/hello python exercices.py
@@ -0,0 +1,35 @@
+#pythonda  dört işlem
+#9 - 3   #6
+#8 * 2.5 #20
+#9 / 2   #4.5
+#9 / -2  #-4.5
+#9 // -2 #-5
+#9 % 2   #1
+#9.0 % 2 #4
+#9 % 2.0 #1.0
+#9 % -2  #-1
+#-9 % 2  #1
+#9 / -2.0 #-4.5
+#4 + 3 * 5 #19
+#(4 + 3) * 5 #35
+#6*3 + 7*4 #46
+#5 + 3 / 4 #5.75
+#5 - 2 * 3 ** 4#-157
+
+#hangisi syntax errola sonuçlanır
+#6 * -----------8 #48 true
+#8 = insanlar #sytax error çünkü iki eşşitir olması lazım
+#((((4 ** 3)))) # 64 true
+#(-(-(-(-5)))) #5 true
+#4 += 7 / 2 #synyax error
+
+#name error örnek
+#name = "Emre"
+#print(age) #name error çünkü bize yaşı vermemış
+
+#name = "Emre"
+#print(nam) #name error bıze "nam"değerini vermemiş
+
+#def sayHello():
+#print("Hello World!")   
+#sayHello()#name 'sayHello' is not defined
