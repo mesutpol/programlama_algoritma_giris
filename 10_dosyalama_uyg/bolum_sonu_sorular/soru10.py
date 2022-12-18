d7211a602b8b5c46b3bc1d01a12cda8d3d5e10de
Quanfr.kslt <115635976+quanfr@users.noreply.github.com>
2022-12-18 20:11:23 +0300

    Aziz Furkan eklendi

--- 10_dosyalama_uyg/bolum_sonu_sorular/soru10.py
+++
@@ -1,7 +0,0 @@
-#soru10: Bir Bölümdeki Öğrencilerin Kişisel Bilgilerini tutan dosya uygulaması programı geliştiriniz
-dosya=open('rehber.txt','a')
-ad=input("Adı-Soyadı: ")
-num =input("Öğrenci Numarası:")
-tel =input("Telefonu :")
-dosya.write(ad+"\t"+num+"\t"+tel+"\n")
-dosya.close()
