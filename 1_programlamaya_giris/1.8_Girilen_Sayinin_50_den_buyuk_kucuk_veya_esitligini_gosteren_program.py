58c57142e2c9f8bd8c61c7f92135d1df42214a72
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2023-10-12 19:56:55 +0300

    Bug Fix
    * Fixed error not working when float is entered as input value.
    
    * Prevented the program from crashing when a string value is entered as input

--- 1_programlamaya_giris/1.8_Girilen_Sayinin_50_den_buyuk_kucuk_veya_esitligini_gosteren_program.py
+++ 1_programlamaya_giris/1.8_Girilen_Sayinin_50_den_buyuk_kucuk_veya_esitligini_gosteren_program.py
@@ -1,11 +1,15 @@
 #Girilen sayının 50'den büyük, küçük veya eşitliğini gösteren program
+import math
 
-a=int(input("tam sayıyı giriniz:"))
-if(50>a):
-    print("girilen sayı 50'den küçük ") #Sayının 50'den küçük olduğunu gösterir.
-if(50<a):
-    print("girilen sayı 50'den büyük") #Sayının 50'den büyük olduğunu gösterir
-if(a==50):
-  print("girilen sayı 50 'ye eşit") #Sayının 50'ye eşit olduğunu gösterir...
+try:
+    inputValue = float(input("Tam sayıyı giriniz: "))
+    flooredValue = int(math.floor(inputValue))
+    if flooredValue < 50:
+        print("Girilen sayı 50'den küçük.")
+    elif flooredValue > 50:
+        print("Girilen sayı 50'den büyük.")
+    else:
+        print("Girilen sayı 50'ye eşit.")
 
-#yorumm
+except ValueError:
+    print("Geçersiz bir sayı girdiniz.")
