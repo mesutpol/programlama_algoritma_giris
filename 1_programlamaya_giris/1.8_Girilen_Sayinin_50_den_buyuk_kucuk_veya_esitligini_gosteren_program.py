20bba067ffadcbfc59ecafe2671d4fdee9529dde
Mehmet Deniz Kumcu <68655812+devmdeniz@users.noreply.github.com>
2023-10-12 20:24:44 +0300

    Add Comment Lines for Explanation

--- 1_programlamaya_giris/1.8_Girilen_Sayinin_50_den_buyuk_kucuk_veya_esitligini_gosteren_program.py
+++ 1_programlamaya_giris/1.8_Girilen_Sayinin_50_den_buyuk_kucuk_veya_esitligini_gosteren_program.py
@@ -1,14 +1,15 @@
 #Girilen sayının 50'den büyük, küçük veya eşitliğini gösteren program
-import math
 
-try:
-    inputValue = float(input("Tam sayıyı giriniz: "))
-    if inputValue < 50:
-        print("Girilen sayı 50'den küçük.")
-    elif inputValue > 50:
-        print("Girilen sayı 50'den büyük.")
-    else:
-        print("Girilen sayı 50'ye eşit.")
+import math #math kütüphanesini ekliyoruz.
 
-except ValueError:
-    print("Geçersiz bir sayı girdiniz.")
+try: #Hata olabilecek kodları try bloğunun içine yazıyoruz.
+    inputValue = float(input("Tam sayıyı giriniz: ")) #Kullanıcıdan sayı alıyoruz.
+    if inputValue < 50: #Eğer girilen sayı 50'den küçükse
+        print("Girilen sayı 50'den küçük.") #Ekrana 50'den küçük yazdırıyoruz.
+    elif inputValue > 50: #Eğer girilen sayı 50'den büyükse
+        print("Girilen sayı 50'den büyük.") #Ekrana 50'den büyük yazdırıyoruz.
+    else: #Eğer girilen sayı 50'ye eşitse
+        print("Girilen sayı 50'ye eşit.") #Ekrana 50'ye eşit yazdırıyoruz.
+
+except ValueError: #Eğer kullanıcı sayı yerine harf veya başka bir karakter girerse
+    print("Geçersiz bir sayı girdiniz.") #Ekrana hata mesajı yazdırıyoruz.
