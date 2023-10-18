215a9428444d6dfeece1b5f6dcbff243cf107918
Oguzhan Demirbas <oguzzh4nn@gmail.com>
2023-10-18 23:30:42 +0300

    Bolum Sonu Soruları 13 Deneme 11

--- 1_programlamaya_giris/bolum_sonu_sorular/1.13_Bolum Sonu Soruları/1.13.2_Matematik_Dilinden_Bilgisayar_Diline_Cevirme_2.py
+++ 1_programlamaya_giris/bolum_sonu_sorular/1.13_Bolum Sonu Soruları/1.13.2_Matematik_Dilinden_Bilgisayar_Diline_Cevirme_2.py
@@ -1,10 +1,10 @@
 '''Sorumuz'''
 '''a+b^1/2+(a+b)^1/2+(a+b)^(1/2)'''
 
-#Sorunun Bize verdiği sabit değerleri giriyoruz
+#Sorunun Bize verdiği sabit değerleri giriyoruz.
 a=9
 b=16
-#Sorudaki ifadeyi bilgisayar diline çeviriyoruz
-print (float ,input(a+b**1/2+(a+b)**1/2+(a+b)**(1/2)))#(1/2) ile köklü ifade belirttip,sonucu ekrana yazdırıyoruz
+
+print (float ,input(a+b**1/2+(a+b)**1/2+(a+b)**(1/2)))#Sorudaki ifadeyi bilgisayar diline çeviriyoruz,(1/2) ile köklü ifade belirtip sonucu ekrana yazdırıyoruz.
 
 
