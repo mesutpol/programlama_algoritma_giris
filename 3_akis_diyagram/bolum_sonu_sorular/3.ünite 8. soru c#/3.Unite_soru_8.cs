8c594ec30217e89352595584091cc2549f5bc965
berktalhaaslan <berktalha7988@gmail.com>
2023-10-27 22:07:04 +0300

    3.ünite 8.soru c#

---
+++ 3_akis_diyagram/bolum_sonu_sorular/3.ünite 8. soru c#/3.Unite_soru_8.cs
@@ -0,0 +1,16 @@
+﻿namespace MyApplication;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("sayi giriniz : ");            //kullanıcından sayı istenir
+        int Sayi = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("yuzde giriniz : ");           //kullanıcıdan yüzde istenir
+        int v = Convert.ToInt32(Console.ReadLine());
+        int Yuzde = v;
+        int Sonuc = Sayi * Yuzde / 100;               //girilen sayının yüzdesi hesaplanır
+        Console.WriteLine("Sonuc : " + Sonuc);        //Sonuç ekrana yazdırılır 
+
+    }
+}
