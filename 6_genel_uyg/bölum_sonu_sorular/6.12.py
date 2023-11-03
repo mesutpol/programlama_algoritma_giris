2e7ab0210c0233ea6965890b2631d0042299cc97
Himmet Can Umutlu <himmet.can.umutlu@yandex.com>
2023-11-03 03:21:48 +0300

    Create 6.12.py

---
+++ 6_genel_uyg/bölum_sonu_sorular/6.12.py
@@ -0,0 +1,8 @@
+#6. Ünite Bölüm Sonu Soruları, 12. Soru: Klavyeden girilen negatif sayıyı, pozitif sayıya çeviren programın akış diyagramını yapınız ve herhangi bir programlama diliyle kodlayınız.
+sayi = float(input("Sayı Giriniz.\n"))
+if sayi < 0 :
+    print(float(sayi * -1))
+else:
+    print("Sayı zaten pozitif.")
+    print("Girdiğiniz sayı:",float(sayi))
+#Himmet Can Umutlu - November 03 - 2023
