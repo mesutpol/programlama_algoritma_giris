57b64543724c314052792a46ad3eb0d10d2189f4
ramazanjk <147262398+ramazanjk@users.noreply.github.com>
2023-11-10 11:35:40 +0000

    dev_2023

---
+++ 6_genel_uyg/6_7_Daireninalanı_ve_hacmi.py
@@ -0,0 +1,8 @@
+#Klavyeden yarıçapı girilen kürenin alanını ve hacmini hesaplayan program.(15.baskı)
+import math
+r=float(input("Yarıçapı giriniz:"))
+Alan=4*(math.pi)*(r**2)
+Hacim=(4*(math.pi)*(r**3))/3
+print("Alan:",Alan,"\nHacim:",Hacim)
+
+
