0c00dd4a902445773b3b9bc76dcdb1c1531acecf
OguzHAN <2023481008@cumhuriyet.edu.tr>
2023-12-01 15:15:06 +0300

    Dev_2023

---
+++ 6_genel_uyg/bölum_sonu_sorular/6._Unite_15.Soru(14.Baskı)_(C#).cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Unite_15._Soru_C__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Kodu yazan Oğuzhan Demirbaş
+            //Kodun yazılma tarihi 26.11.2023
+
+            Console.Write("Bir 'E' değeri girin :");//23.Kod satırına kadar bize soruda verilen değişkenleri kullanıcıdan istedik ve double türüne çevirdik.
+            double E = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Bir 'R1' değeri girin :");
+            double R1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Bir 'R2' değeri girin :");
+            double R2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Bir 'R3' değeri girin :");
+            double R3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Bir 'R4' değeri girin :");
+            double R4 = Convert.ToDouble(Console.ReadLine());
+
+            double I = E / (R1 + R2 + R3 + R4);
+            double VR1 = R1 * I;
+            double PR2 = I * I * R2;
+
+            Console.WriteLine($"VR1 :{VR1} \nPR2 :{PR2} \nI :{I}");//Burada süslü parantez içine değişkenleri çağırdık.
+            Console.ReadKey();
+        }
+    }
+}
