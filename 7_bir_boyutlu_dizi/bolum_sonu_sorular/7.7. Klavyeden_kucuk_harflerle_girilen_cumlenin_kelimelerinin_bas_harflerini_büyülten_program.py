31b5d13c9d0a3fb2d578964dae5246828f309698
Emir Kalyoncu <kyleunofficial@hotmail.com>
2023-12-15 00:50:36 +0300

    7.7. Klavyeden_kucuk_harflerle_girilen_cumlenin_kelimelerinin_bas_harflerini_büyülten_program_ve_akis_diyagrami

---
+++ 7_bir_boyutlu_dizi/bolum_sonu_sorular/7.7. Klavyeden_kucuk_harflerle_girilen_cumlenin_kelimelerinin_bas_harflerini_büyülten_program.py
@@ -0,0 +1,8 @@
+# 7. Ünite 7. soru:
+# Klavyeden küçük harflerle girilen bir cümlede kelimelerin sadece ilk harflerini büyütüp yeniden yazdıran programın
+# akış diyagramını çizip yazılımını herhangi bir programlama dili ile yazınız.
+
+cumle = input("Cümle giriniz: ")
+c = cumle.title()
+print(c)
+
