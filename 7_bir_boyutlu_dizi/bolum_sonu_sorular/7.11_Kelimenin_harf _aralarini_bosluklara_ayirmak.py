02bfc9279a286f304840016379bd9f918bf29817
Denizcan Özata <147205090+denizozata0@users.noreply.github.com>
2023-12-15 00:30:07 +0300

    7.11

---
+++ 7_bir_boyutlu_dizi/bolum_sonu_sorular/7.11_Kelimenin_harf _aralarini_bosluklara_ayirmak.py
@@ -0,0 +1,8 @@
+# 7.ünite 11.soru:
+# Klavyeden girilen bir kelimenin harfleri arasına birer tane boşluk koyarak yeniden yazdıran programın 
+# akış diyagramını çizip yazılımını yazınız.
+
+
+kelime = input("Kelimeyi giriniz: ")
+degisken = " ".join(kelime)
+print(degisken)
