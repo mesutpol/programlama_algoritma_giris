11683ec893c1acc19b515e5bb1aa877e9ff03561
OguzHAN <2023481008@cumhuriyet.edu.tr>
2023-12-14 22:12:38 +0300

    6_Unite_18._Soru_(14.Baskı_py,C#,Akis_Di,)

---
+++ 6_genel_uyg/bölum_sonu_sorular/6_Unite_18._Soru_(14.Baskı_py,C#,Akis_Di.)/6_Unite_18._Soru_(C#).cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Unite_18._Soru__C__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir sayı giriniz");
+            int Sayi = Convert.ToInt32(Console.ReadLine());
+            string Sayi_str = Convert.ToString(Sayi);
+
+            List<int> liste = new List<int>();
+
+            for (int i=0; i < Sayi_str.Length; i++)
+            {
+                liste.Add(Convert.ToInt32(Sayi_str[i]));
+            }
+            int Toplam = liste.Sum();
+            Console.Write("Toplam :",Toplam);
+            Console.ReadKey();
+
+
+
+
+
+        }
+    }
+}
