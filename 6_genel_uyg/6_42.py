d42fc031d1d871a643c7c968ed1f42c1a435682a
ramazanjk <147262398+ramazanjk@users.noreply.github.com>
2023-12-18 23:30:13 +0300

    Update 6_42.py

--- 6_genel_uyg/6_42.py
+++ 6_genel_uyg/6_42.py
@@ -1,3 +1,6 @@
+#Kodu yazan-->Ramazan Polat
+#Yazılma tarihi-->18.12.2023
+#Soru-->Klavyeden girilen iki pozitif tam sayıyı -çarpma operatörü kullanmadan- çarpan program
 a = int(input("İlk pozitif tamsayıyı giriniz:"))
 b = int(input("İkinci pozitif tamsayıyı giriniz:"))
 c=0
