e14545b2533b52d3d8dc371dac5d2b3c7eac83d4
Efe Mehmet <cyberbilgilendirme@gmail.com>
2023-12-29 01:35:50 +0300

    7.16_Girilen_doğum_tarihine_göre_kişinin_burcunu_yazdıran_kod

---
+++ 7_bir_boyutlu_dizi/bolum_sonu_sorular/7.16_Girilen_doğum_tarihine_göre_kişinin_burcunu_yazdıran_kod/7.16_Girilen_doğum_tarihine_göre_kişinin_burcunu_yazdıran_kod.py
@@ -0,0 +1,22 @@
+
+#1. Burçları bir diziye yerleştirin.
+#2. Kullanıcıdan doğum tarihini isteyin.
+#3. Doğum tarihinin gününü ve ayını alın.
+#4. Doğum tarihine göre burcu bulun.
+#5. Burcu ekrana yazdırın.
+
+
+burçlar = ["Oğlak", "Kova", "Balık", "Koç", "Boğa", "İkizler", "Yengeç", "Aslan", "Başak", "Terazi"]
+
+# Kullanıcıdan doğum tarihini isteyin
+gün = int(input("Doğum gününüzün gününü girin: "))
+ay = int(input("Doğum gününüzün ayını girin: "))
+
+# Doğum tarihine göre burcu bulun
+if gün <= 20:
+    burç = burçlar[ay - 1]
+else:
+    burç = burçlar[ay]
+
+# Burcu ekrana yazdırın
+print("Burcunuz:", burç)
